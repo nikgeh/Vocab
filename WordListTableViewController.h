39b5c5405d3794cdb3581a2e891c029d7c414fdf
Nik Geh <nikgeh@gmail.com>
2011-07-28 21:33:08 -0700

    Create blank WordListTableViewController

---
+++ WordListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  WordListTableViewController.h
+//  Vocabulous
+//
+//  Created by Nik on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WordListTableViewController : UITableViewController {
+    
+}
+
+@end
