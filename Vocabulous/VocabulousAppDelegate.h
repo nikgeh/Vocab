36ad64c6aee45387697930cc4f8bc1520c77f651
Nik Geh <nikgeh@gmail.com>
2011-07-28 21:31:51 -0700

    Initial Commit

---
+++ Vocabulous/VocabulousAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  VocabulousAppDelegate.h
+//  Vocabulous
+//
+//  Created by Nik on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VocabulousAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
